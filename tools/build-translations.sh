6f119cccaa9bf491abbd27528a49ed2cd4f8dc58
Aurélien PIERRE <contact@aurelienpierre.com>
2025-04-24 17:26:26 +0200

    Minor improvements

--- tools/build-translations.sh
+++ tools/build-translations.sh
@@ -15,7 +15,6 @@ fi
 
 # Get the config file
 po4a_conf="po/po4a.conf"
-
 if ! test -f $po4a_conf || [ ! -s $po4a_conf ]; then
     echo "The file $po4a_conf is empty or don't exist. Run `./tools/update-translations.sh` first"
     exit 1
@@ -33,7 +32,7 @@ else
   exit 1
 fi
 
-# Always remove all languages in case disabled languages have changed
+# Always remove all languages translations in case the set of disabled languages has changed
 echo "Removing translations for all languages"
 po4a --rm-translations --no-update --no-translations --verbose "$po4a_conf"
 
@@ -51,5 +50,5 @@ sed -i "/\[po4a_langs\].*/c\[po4a_langs] $languages" $po4a_conf
 # Generate only the target languages
 if [ "$mode" = "add" ]; then
   echo "Generating translations for $languages"
-  po4a --keep 0 --no-update --verbose $po4a_conf
+  po4a --keep 0 --no-update --verbose "$po4a_conf"
 fi
